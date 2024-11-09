6965c85ab86c930bfefd14a275036e9e70bbf8f9
Uvil1616 <71354625+Uvil1616@users.noreply.github.com>
2024-11-09 02:18:16 -0500

    Update LoadBalencing.py
    replaced .size() with len() instead and fixed method to determine quickest queue within each subset of queues

--- LoadBalencing.py
+++ LoadBalencing.py
@@ -32,45 +32,50 @@ class loadBalencing:
         for m in messages:
     
             #variables used to determine which queue message should be added to
-            prevCapacity = len(list[0])
+            prevCapacity = 0
             quickest = 0
     
             #each priority has own dedicated section of list
             #higher priorities have more reserved list
             #Priority 5 has 40%
             if m.priority == 5:
+                prevCapacity = len(list[0])
                 for i in range(math.floor(len(list)*0.4)):
-                    if prevCapacity >= list[i].size:
+                    if prevCapacity >= len(list[i]):
                         quickest = i
-                        prevCapacity = list[i].size
+                        prevCapacity = len(list[i])
                 list[quickest].addMessage(m)
             #Priority 4 has 20%
             elif m.priority == 4:
+                prevCapacity = len(list[math.floor(len(list)*0.4)])
                 for i in range(math.floor(len(list)*0.4), math.floor(len(list)*0.6)):
-                    if prevCapacity >= list[i].size:
+                    if prevCapacity >= len(list[i]):
                         quickest = i
-                        prevCapacity = list[i].size
+                        prevCapacity = len(list[i])
                 list[quickest].addMessage(m)
             #Priority 3 has 20%
             elif m.priority == 3:
+                prevCapacity = len(list[math.floor(len(list)*0.6)])
                 for i in range(math.floor(len(list)*0.6), math.floor(len(list)*0.8)):
-                    if prevCapacity >= list[i].size:
+                    if prevCapacity >= len(list[i]):
                         quickest = i
-                        prevCapacity = list[i].size
+                        prevCapacity = len(list[i])
                 list[quickest].addMessage(m)
             #Priority 2 has 10%
             elif m.priority == 2:
+                prevCapacity = len(list[math.floor(len(list)*0.8)])
                 for i in range(math.floor(len(list)*0.8), math.floor(len(list)*0.9)):
-                    if prevCapacity >= list[i].size:
+                    if prevCapacity >= len(list[i]):
                         quickest = i
-                        prevCapacity = list[i].size
+                        prevCapacity = len(list[i])
                 list[quickest].addMessage(m)
             #Priority 1 has 10%
             else:
+                prevCapacity = len(list[math.floor(len(list)*0.9)])
                 for i in range(math.floor(len(list)*0.9), math.floor(len(list))):
-                    if prevCapacity >= list[i].size:
+                    if prevCapacity >= len(list[i]):
                         quickest = i
-                        prevCapacity = list[i].size
+                        prevCapacity = len(list[i])
                 list[quickest].addMessage(m)
         return None
 
