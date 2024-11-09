2607fb03e26708d1c3366c2262fb5973a50f93e5
Laura <lchin039@uottawa.ca>
2024-11-09 10:24:44 -0500

    Update cloudQueue.py
    added write up and added event list to calculate wait time properly

--- cloudQueue.py
+++ cloudQueue.py
@@ -3,35 +3,60 @@ import heapq
 import Message
 
 class cloudQueue():
-    n_servers: int
-    service_time: np.random.default_rng
-    service_time_mean: float
-    queue: list
-    def __init__(self, n_servers: int, service_time: np.random.default_rng, service_time_mean: float, queue = []):
+    """
+    A class that creates an M/M/n/inf queue with exponentially distributed service time
+    """
+    n_servers: int #number of servers accepting customers from the queue
+    service_time: np.random.default_rng #specifies the numpy method used to generate the random numbers
+    service_time_mean: float #the mean of the exponential distribution used to calculate the service time
+    processed: list #a list of all customers in the queue system - sorted with a min heap
+    event_list: list
+    def __init__(self, n_servers: int,  service_time_mean: float, service_time = np.random.default_rng(), processed = [], event_list = []):
         self.n_servers = n_servers
         self.service_time = service_time
         self.service_time_mean = service_time_mean
-        self.queue = queue 
+        self.processed = processed
+        self.event_list = event_list
 
 
     def addMessage(self, message: Message) -> None: 
-        ##do calculate wait time, service time, and departure time
-        next_departure = 0 if len(self.queue) == 0 else self.queue[0]
+        #calculates the message's service time using an exponential distribution with the queue's mean service time 
         message.cloud_service_time = self.service_time.exponential(self.service_time_mean)
-        message.cloud_wait_time = 0 if len(self.queue) < self.n_servers else max(0, next_departure.fog_departure_time - message.cloud_arrival_time)
+
+        #########Calculation of Wait Time ###########
+        #checks the event list, if the event list is full then remove, the next to leave
+        next_departure = None if len(self.event_list) < self.n_servers else heapq.heappop(self.event_list)
+
+        #checks if more messages are in the system than are being served (meaning they are in the queue), if there is a queue, the wait time is calculated from the next customer to depart the system
+        message.cloud_wait_time = 0 if next_departure == None else max(0, next_departure.cloud_departure_time - message.cloud_arrival_time)
+        #calculates the departure time from the message's arrival time, service time, and wait time (arrival time is calculated in main based on the message's departure time from the previous queue + lag time)
         message.cloud_departure_time = message.cloud_arrival_time + message.cloud_service_time + message.cloud_wait_time
-        heapq.heappush(self.queue, message)
+        #sets the message's sorting criteria to the cloud departure_time
+        message.current_departure_time = message.cloud_departure_time
+
+        #sorts the event list
+        heapq.heappush(self.event_list, message)
+        #adds the message that left the eventlist into the processed list
+        if next_departure != None:
+            self.processed.append(next_departure)
+
         return None
 
 
 
     def removeMessage(self, current_time: int) -> Message:
-        departing = []
-        for i in range(0, len(self.queue)):
-            next_departure = self.queue[0]
-            if next_departure.cloud_departure_time <= current_time:
-                depart = heapq.heappop(self.queue)
-                departing.append(depart)
-            else:
-                break
+        departing = [] #a list that contains all messages that left the system between the previous and current time interval
+
+        for i in range(0, self.n_servers):
+            if self.event_list[0].cloud_departure_time <= current_time:
+                self.processed.append(heapq.heappop(self.event_list))
+
+        for i in range(0, len(self.processed)):
+            if(self.processed[0].cloud_departure_time <= current_time):
+                departing.append(self.processed.pop(0))
+            #next_departure = self.queue[0] #peeks at the item at the top of the queue
+            #if next_departure.cloud_departure_time <= current_time: #if the fog departure time is less or equal to current time, message leaves the system
+            #    depart = heapq.heappop(self.queue)
+            #    departing.append(depart)
+
         return departing
