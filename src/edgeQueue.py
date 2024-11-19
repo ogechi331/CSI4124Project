4eca64ce2aa4cc3621f2949f0cb0b46b4c153f76
Laura <lchin039@uottawa.ca>
2024-11-19 15:33:09 -0500

    Update edgeQueue.py
    updated edgeQueue so it should properly take and send out messages

--- src/edgeQueue.py
+++ src/edgeQueue.py
@@ -5,18 +5,25 @@ import numpy as np
 class EdgeQueue:
     def __init__(self, capacity, serviceRate):
         self.capacity = capacity  # Max capacity of the queue
+        self.eventList = []
         self.queue = []  # List to hold messages in the
         self.serviceRate = serviceRate
 
-    def addMessage(self, message, current_time):
+    def addMessage(self, message):
 
         if self.checkCapacity():
-            message.wait_time = current_time  # Assuming wait time is calculated as current_time
+            if len(self.queue) > 0:
+                message.edge_wait_time = (self.queue[0] - message.edge_arrival_time) 
+                self.queue.append(heapq.heappop(self.eventList))
+            else:
+                message.edge_wait_time = 0
+            # message.wait_time = current_time  # Assuming wait time is calculated as current_time
 
-            service_time = np.random.exponential(1 / self.serviceRate)
-            message.current_departure_time = current_time + service_time  # Set departure time based on processing time
+            message.edge_service_time = np.random.exponential(self.serviceRate)
+            message.edge_departure_time= message.edge_arrival_time + message.edge_service_time + message.edge_wait_time  # Set departure time based on processing time
             # ensure the messages are ordered from lowest to greatest departure when adding messages
-            heapq.heappush(self.queue, message)
+            message.current_departure_time = message.edge_departure_time
+            heapq.heappush(self.eventList, message)
             return True  # Mess age added successfully
         else:  # Increase global dropout counter if queue is full
             return False  # Message not added due to full capacity
@@ -26,6 +33,17 @@ class EdgeQueue:
         """
         return len(self.queue) < self.capacity
 
+    def removeMessage(self, current_time: int) -> list:
+        departing = []
+        for i in range(0, len(self.eventList)):
+            if self.eventList[0].edge_departure_time <= current_time:
+                self.queue.append(heapq.heappop(self.eventList))
+        for i in range(0, len(self.queue)):
+            if (self.queue[0].edge_departure_time <= current_time):
+                departing.append(self.edge_departure_time.pop(0))
+        return departing
+
+        '''
     def removeMessage(self, current_time):
         if self.queue and self.queue[0].departure_time <= current_time:
             return self.queue.pop(0)
@@ -41,3 +59,4 @@ class EdgeQueue:
     def __gt__(self, other):
         """Compare two EdgeQueue objects based on the next message's departure time."""
         return self.queue[0] > other.queue[0]
+        '''
